02cfc5600b25e436fcb1c334e73980ed9821689b
Justyn Mencl <men19033@byui.edu>
2022-01-15 14:58:40 -0700

    changed to dotnet 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSE210_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
