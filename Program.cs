982cc9c9da0fb1ccb4e9a564bc0f050f5575721e
Justyn Mencl <men19033@byui.edu>
2022-01-15 20:47:13 -0700

    changes and game

--- Program.cs
+++ Program.cs
@@ -5,8 +5,94 @@ namespace CSE210_01
     class Program
     {
         static void Main(string[] args)
+        {   // declaring function calls as variables
+            string player = NextPlayer("");
+            string[] board = CreateBoard();
+
+            while (!IsFinished(board) && !HasWinner(board))
+            /* loops until there is a winner or the board is full */
+            {
+                DisplayBoard(board);
+                MakeMove(player, board);
+                player = NextPlayer(player);
+            }
+
+            DisplayBoard(board);
+            Console.WriteLine("\nGood game. Thanks for playing.");
+        }
+
+        static string[] CreateBoard()
+        /* Creates a board with 9 squares. */
         {
-            Console.WriteLine("Hello World!");
+            string[] board = new string[9];
+            for (int i = 0; i < 9; i++)
+            {
+                board[i] = (i + 1).ToString();
+            }
+            return board;
+        }
+
+        static void DisplayBoard(string[] board)
+        /* Displays the squares into a 3x3 grid. */
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+        }
+
+        static bool HasWinner(string[] board)
+        {
+            return ((board[0] == "x" && board[1] == "x" && board[2] == "x") 
+                    || (board[3] == "x" && board[4] == "x" && board[5] == "x") 
+                    || (board[6] == "x" && board[7] == "x" && board[8] == "x") 
+                    || (board[0] == "x" && board[3] == "x" && board[6] == "x") 
+                    || (board[1] == "x" && board[4] == "x" && board[7] == "x") 
+                    || (board[2] == "x" && board[5] == "x" && board[8] == "x") 
+                    || (board[0] == "x" && board[4] == "x" && board[8] == "x")
+                    || (board[2] == "x" && board[4] == "x" && board[6] == "x")
+                    || (board[0] == "o" && board[1] == "o" && board[2] == "o") 
+                    || (board[3] == "o" && board[4] == "o" && board[5] == "o") 
+                    || (board[6] == "o" && board[7] == "o" && board[8] == "o") 
+                    || (board[0] == "o" && board[3] == "o" && board[6] == "o") 
+                    || (board[1] == "o" && board[4] == "o" && board[7] == "o") 
+                    || (board[2] == "o" && board[5] == "o" && board[8] == "o") 
+                    || (board[0] == "o" && board[4] == "o" && board[8] == "o")
+                    || (board[2] == "o" && board[4] == "o" && board[6] == "o"));
+        }
+
+        static bool IsFinished(string[] board)
+        {
+            bool result = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] != "x" && board[i] != "o")
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        static void MakeMove(string player, string[] board)
+        {
+            Console.Write($"\n{player}'s turn to choose a square (1-9): ");
+            int square = Convert.ToInt32(Console.ReadLine());
+            board[square - 1] = player;
+        }
+
+        static string NextPlayer(string current)
+        {
+            string player = "o";
+            if (current == "" || current == "o")
+            {
+                player = "x";
+            }
+            return player;
+        
         }
     }
 }
